5eeadd4aefdf8ced107dee98814501ce702fa4f3
Christos Stamatopoulos <80109501+stamchris@users.noreply.github.com>
2022-10-30 14:36:22 +0200

    AddPoints and ResetScore function

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ const score1 = document.querySelector('#p1');
 const score2 = document.querySelector('#p2');
 const add1 = document.querySelector('#b1');
 const add2 = document.querySelector('#b2');
+const reset = document.querySelector('#reset');
 
 
 let counter1 = 0;
 let counter2 = 0;
 
-add1.addEventListener('click', () => addPoints(counter1));
-add2.addEventListener('click', () => addPoints(counter2));
+add1.addEventListener('click',addPoints);
+add2.addEventListener('click',addPoints);
+reset.addEventListener('click',resetScore);
 
 
-function addPoints(counter){
-    if(counter === counter1){
-        counter1 < 7 ? counter1 ++ : add1.disabled = true;
-        score1.innerText = counter1; 
-    }else{
-        counter2 < 7 ? counter2 ++ : add2.disabled = true; 
-        score2.innerText = counter2; 
-    }
+function addPoints() {
+		if (this.id === 'b1'){
+			counter1 < 7 ? counter1++ : add1.disabled = true;
+			score1.innerText = counter1;
+		}else{
+			counter2 < 7 ? counter2++ : add2.disabled = true;
+			score2.innerText = counter2;
+		}
+}
+
+function resetScore(){
+	counter1 = 0;
+	counter2 = 0;
+	score1.innerText = 0;
+	score2.innerText = 0;
+	add1.disabled = false;
+	add2.disabled = false;
 }
